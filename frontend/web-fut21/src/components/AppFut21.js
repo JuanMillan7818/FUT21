5fbb737bf7f7e5f89b2336233927e7be90572a29
Juan Pablo <juan.holgin@correounivalle.edu.co>
2022-01-06 08:15:43 -0500

    Plantilla de interfaz

--- frontend/web-fut21/src/components/AppFut21.js
+++ frontend/web-fut21/src/components/AppFut21.js
@@ -3,9 +3,18 @@ import "../styles/style.css";
 export const AppFut21 = () => {
   return (
     <>
-      <div className="container">
-        <div className="layer">
-          <h1 data-text="FIFA hola">FIFA&nbsp;hola</h1>
+      <div className="layer-back">
+        <div className="container">
+          <div className="layer">
+            <div className="title-main">
+              <h1>FIFA&nbsp;21&nbsp;Ultimate&nbsp;Team</h1>
+            </div>
+          </div>
+        </div>
+        <div className="imgBx"></div>
+        <div>
+          <div className="card"></div>
+          <div className="imgBx"></div>
         </div>
       </div>
     </>
